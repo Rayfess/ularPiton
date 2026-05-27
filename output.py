7ea16be85e0317cdbbe105896ec30876c6a0c4e0
Rayfess <153087368+Rayfess@users.noreply.github.com>
2026-05-27 09:25:39 +0700

    add ouput use case

---
+++ output.py
@@ -0,0 +1,18 @@
+
+"""
+Examples use cases of Output in Python
+"""
+
+x = "ABC"
+y = 5 + int("2") #str("2") #casting
+z = "im " + str("5") + " years old"
+
+print("right after this One ->", end=" ")
+print("will print on the same line.")
+
+print(5)
+print(y)
+print(x)
+print(z)
+
+print("Excuse me sir, im", 30, "years Old" )
